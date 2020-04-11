40807e9dcce2e897200f0562f96ab5e1cb151810
Amin NAIRI <nairi.amin@gmail.com>
2020-04-12 00:53:13 +0200

    :tada: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "@aminnairi"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    rules: {
+        "new-cap": "off"
+    }
+};
