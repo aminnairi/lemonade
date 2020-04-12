97161197c82ac3cb79ac04f0c0f20febf63d970a
Amin NAIRI <nairi.amin@gmail.com>
2020-04-12 12:29:27 +0200

    :rotating_light: fixed linter issues

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,15 @@
-"use strict";
-
-import { resolve } from "path";
-import { terser } from "rollup-plugin-terser";
+import {resolve} from "path";
+import {terser} from "rollup-plugin-terser";
 
 export default {
     input: resolve("lemonade.js"),
 
-    plugins: [
-        terser()
-    ],
-
     output: {
         file: resolve("lemonade.min.js"),
         format: "cjs"
-    }
+    },
+
+    plugins: [
+        terser()
+    ]
 };
